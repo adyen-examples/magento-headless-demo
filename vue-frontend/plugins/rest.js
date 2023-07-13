87bd91a2bf2fd04c42a3171997c00a8b89b3c96b
carlospe <carlos.perales@adyen.com>
2023-07-13 12:30:27 +0200

    small css fix and start implementing most REST functions

--- vue-frontend/plugins/rest.js
+++ vue-frontend/plugins/rest.js
@@ -1,14 +1,15 @@
-export async function sendRestReq(data, endpoint) {
+export async function sendRestReq(data, endpoint, method) {
   try {
     const host = process.env.BASE_URL;
+    // const bearer = authRequired ? process.env.BEARER_TOKEN : '';
     const bearer = process.env.BEARER_TOKEN;
+    const reqMethod = method;
 
     let response = await fetch(host + endpoint, {
-      method: 'POST',
+      method: reqMethod,
       mode: 'cors',
       headers: {
         "Content-Type": "application/json",
-        'Content-Length': data.length,
         Authorization: 'Bearer '+ bearer,
         'Origin': host,
       },
@@ -29,33 +30,43 @@ export async function getCartId() {
   const endpoint ='/rest/default/V1/guest-carts';
   const data = JSON.stringify({});
 
-  const response = await this.sendRestReq(data, endpoint);
-
-  console.log(response);
-
+  const response = await this.sendRestReq(data, endpoint, 'POST');
+  return response;
 }
 
-// TODO
-export async function addProductsToCart(cartId) {
+export async function addProductsToCart(cartId, quote) {
   const endpoint ='/rest/default/V1/guest-carts/' + cartId + '/items';
   const data = JSON.stringify({
     "cartItem" : {
       "qty": 1,
       "sku": "24-MB02",
-      "quote_id": "4"
+      "quote_id": quote
     }
   });
-
-  const response = await this.sendRestReq(data, endpoint);
-  console.log(response);
+  const response = await this.sendRestReq(data, endpoint, 'POST');
+  return response;
 }
 
-// TODO
+export async function removeItemFromCart(cartId, productId) {
+  try {
+    // Add items to cart
+    const endpoint ='/rest/default/V1/guest-carts/' + cartId + '/items/' + productId;
+    const data = JSON.stringify({
+      "cartId": cartId,
+      "itemId": productId,
+    });
+    const response = await this.sendRestReq(data, endpoint, 'DELETE');
+    return response;
+  } catch (error) {
+    console.error(error);
+  }
+}
+
 export async function queryCart(cartId) {
   try {
-    const endpoint ='/rest/default/V1/guest-carts/' + cartId + '/items';
+    const endpoint ='/rest/default/V1/guest-carts/' + cartId;
     const data = null;
-    const response = await this.sendRestReq(data, endpoint);
+    const response = await this.sendRestReq(data, endpoint, 'GET');
     return response;
 
   } catch (error) {
@@ -63,16 +74,13 @@ export async function queryCart(cartId) {
   }
 }
 
-// TODO
+// TO TEST
 export async function searchProducts(searchString) {
   try {
-    const endpoint =`/rest/default/V1/products/?
-        searchCriteria[filter_groups][0][filters][0][field]=name&
-        searchCriteria[filter_groups][0][filters][0][value]=` + searchString + `&
-        searchCriteria[filter_groups][0][filters][0][condition_type]=like;`
-    const data = JSON.stringify({});
+    const endpoint =`/rest/default/V1/products/?searchCriteria[filter_groups][0][filters][0][field]=name&searchCriteria[filter_groups][0][filters][0][value]=` + searchString + `&searchCriteria[filter_groups][0][filters][0][condition_type]=like;`
+    const data = null;
 
-    const response = await this.sendRestReq(data, endpoint);
+    const response = await this.sendRestReq(data, endpoint, 'GET');
     console.log(response);
     return response;
   } catch (error) {
@@ -80,20 +88,6 @@ export async function searchProducts(searchString) {
   }
 }
 
-// TODO
-export async function removeItemFromCart(cartId, productId) {
-  try {
-    // Add items to cart
-    const endpoint ='/rest/default/V1/guest-carts/' + cartId + '/items';
-    const data = null;
-
-    const response = await this.sendRestReq(data, endpoint);
-    return response.data.removeItemFromCart;
-
-  } catch (error) {
-    console.error(error);
-  }
-}
 
 // TODO
 export async function setGuestEmailOnCart(cartId, shopperEmail) {
@@ -109,43 +103,77 @@ export async function setGuestEmailOnCart(cartId, shopperEmail) {
   }
 }
 
-// TODO
 export async function setShippingAddressesOnCart(cartId, shippingAddress) {
   try {
-    const endpoint ='/rest/default/V1/guest-carts/' + cartId + '/items';
-    const data = null;
+    console.log(shippingAddress);
+    const endpoint ='/rest/default/V1/guest-carts/' + cartId + '/shipping-information';
+    const data = JSON.stringify({
+      addressInformation: {
+        shipping_address: {
+          firstname: shippingAddress.firstName,
+          lastname: shippingAddress.lastName,
+          street: [shippingAddress.street],
+          postcode: shippingAddress.postcode,
+          city: shippingAddress.city,
+          region: shippingAddress.region,
+          country_id: shippingAddress.country_code,
+          telephone: shippingAddress.telephone,
+        },
+        shipping_method_code: shippingAddress.shipping_method_code,
+        shipping_carrier_code: shippingAddress.shipping_carrier_code,
+      }
+    });
 
-    const response = await this.sendRestReq(data, endpoint);
+    const response = await this.sendRestReq(data, endpoint, 'POST');
+    return response;
 
   } catch (error) {
     console.error(error);
   }
 }
 
-// TODO
 export async function setBillingAddressOnCart(cartId, billingAddress) {
   try {
-    const endpoint ='/rest/default/V1/guest-carts/' + cartId + '/items';
-    const data = null;
-
-    const response = await this.sendRestReq(data, endpoint);
-    return response.data.setBillingAddressOnCart;
+    const endpoint ='/rest/default/V1/guest-carts/' + cartId + '/billing-address';
+    const data = JSON.stringify({
+      address: {
+        firstname: billingAddress.firstName,
+        lastname: billingAddress.lastName,
+        street: [billingAddress.street],
+        postcode: billingAddress.postcode,
+        city: billingAddress.city,
+        region: billingAddress.region,
+        country_id: billingAddress.country_code,
+        telephone: billingAddress.telephone,
+      }
+    });
+    const response = await this.sendRestReq(data, endpoint, 'POST');
+    return response;
 
   } catch (error) {
     console.error(error);
   }
 }
 
-// TODO
-export async function setShippingMethodsOnCart(cartId, method) {
+export async function estimateShippingMethods(cartId, billingAddress) {
   try {
-    const endpoint ='/rest/default/V1/guest-carts/' + cartId + '/items';
-    const data = null;
+    const endpoint ='/rest/default/V1/guest-carts/' + cartId + '/estimate-shipping-methods';
+    const data = JSON.stringify({
+      address: {
+        firstname: billingAddress.firstName,
+        lastname: billingAddress.lastName,
+        street: [billingAddress.street],
+        postcode: billingAddress.postcode,
+        city: billingAddress.city,
+        region: billingAddress.region,
+        country_id: billingAddress.country_code,
+        telephone: billingAddress.telephone,
+      }
+    });
+    const response = await this.sendRestReq(data, endpoint, 'POST');
+    return response;
 
-    const response = await this.sendRestReq(data, endpoint);
-    return response.data.setShippingMethodsOnCart;
-
-  } catch(error) {
+  } catch (error) {
     console.error(error);
   }
 }
@@ -153,10 +181,13 @@ export async function setShippingMethodsOnCart(cartId, method) {
 // TODO
 export async function getAdyenPaymentMethods(cartId) {
   try {
-    const endpoint ='/rest/default/V1/guest-carts/' + cartId + '/items';
-    const data = null;
+    const endpoint ='/rest/default/V1/guest-carts/' + cartId + '/retrieve-adyen-payment-methods';
+    const data = JSON.stringify({
+      "cart_id": cartId,
+      "shopperLocale": "en_US",
+    });
 
-    const response = await this.sendRestReq(data, endpoint);
+    const response = await this.sendRestReq(data, endpoint, 'POST');
     return response;
 
   } catch(error) {
@@ -170,83 +201,24 @@ export async function setPaymentMethodAndPlaceOrder(cartId, stateData) {
     let data = "";
     if (stateData.paymentMethod.type === "scheme") {
       data = JSON.stringify({
-        query: `mutation setPaymentMethod($cartId: String! $stateData: String!) {
-            setPaymentMethodOnCart(
-                input: {
-                cart_id: $cartId
-                payment_method: {
-                    code:`+ '"' + "adyen_cc" + '"'+ `,
-                    adyen_additional_data_cc: {
-                    cc_type:` + '"' + stateData.paymentMethod.brand + '"' + `,
-                    stateData: $stateData
-                    }
-                }
-                }) {
-                cart {
-                    selected_payment_method {
-                    code
-                    title
-                    }
-                }
-                }
-                placeOrder(
-                input: {
-                    cart_id: $cartId
-                }) {
-                    order {
-                    order_id
-                    adyen_payment_status {
-                        isFinal
-                        resultCode
-                        additionalData
-                        action
-                    }
-                    }
-                }
-                }`,
-        variables: {cartId: cartId, stateData: JSON.stringify(stateData)},
+        method: "adyen_cc",
+        additional_data: {
+          cc_type: stateData.paymentMethod.brand,
+          stateData: stateData,
+        }
       });
     } else {
       let brand = stateData.paymentMethod.type;
       data = JSON.stringify({
-        query: `mutation setPaymentMethod($cartId: String! $stateData: String!) {
-            setPaymentMethodOnCart(
-                input: {
-                cart_id: $cartId
-                payment_method: {
-                    code:` + '"' + "adyen_hpp" + '"' + `,
-                    adyen_additional_data_hpp: {
-                    brand_code:` + '"' + brand + '"' + `,
-                    stateData: $stateData
-                    }
-                }
-                }) {
-                cart {
-                    selected_payment_method {
-                    code
-                    title
-                    }
-                }
-                }
-                placeOrder(
-                input: {
-                    cart_id: $cartId
-                }) {
-                    order {
-                    order_id adyen_payment_status {
-                        isFinal
-                        resultCode
-                        additionalData
-                        action
-                    }
-                    }
-                }
-                }`,
-        variables: {cartId: cartId, stateData: JSON.stringify(stateData) },
+        method: "adyen_hpp",
+        additional_data: {
+          brand_code: brand,
+          stateData: null,
+        }
       });
     }
     const response = await this.sendGraphQLReq(data);
-    return response.data.placeOrder;
+    return response;
 
   } catch(error){
     console.error(error);
